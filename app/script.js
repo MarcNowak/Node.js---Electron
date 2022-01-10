be2d8d5f953975f1b98ec47e05291dfd1e5e2b7f
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-01-10 21:53:08 +0100

    Add 26.4 - TASK - Node.js - Electron - v2

--- app/script.js
+++ app/script.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import AppDescription from './AppDescription';
 
+
+class AppDescription extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+        <p>This app will help you track your time and inform you when it's time to rest.</p>
+      </div>
+    );
+  }
+};
 
 class App extends React.Component {
   state = {
